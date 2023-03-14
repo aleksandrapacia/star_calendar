8a9c28a3181ab47b5ed986b1daf9231061608e19
Aleksandra Pacia <olusiaop05@gmail.com>
2023-03-14 19:42:53 +0100

    primary color changed, home page add

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
