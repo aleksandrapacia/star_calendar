ecb8bcc26da92e8e5d3c58fd61eefe52de8e42ab
Aleksandra Pacia <olusiaop05@gmail.com>
2023-03-26 00:20:54 +0100

    blank floating action button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -31,42 +31,46 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PageView(
-          controller: _controlPages,
-          children: _screens,
-          onPageChanged: _onPageChanged,
-          physics: NeverScrollableScrollPhysics(),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            // index = 0
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.notes_outlined,
-                color: selectedIndex == 0 ? Colors.blue : Colors.grey,
-              ),
-              label: 'Notes',
+      body: PageView(
+        controller: _controlPages,
+        children: _screens,
+        onPageChanged: _onPageChanged,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          // index = 0
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notes_outlined,
+              color: selectedIndex == 0 ? Colors.blue : Colors.grey,
             ),
-            // index = 1
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.calendar_view_day_outlined,
-                color: selectedIndex == 1 ? Colors.blue : Colors.grey,
-              ),
-              label: 'Calendar',
+            label: 'Notes',
+          ),
+          // index = 1
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.calendar_view_day_outlined,
+              color: selectedIndex == 1 ? Colors.blue : Colors.grey,
             ),
-            // index = 2
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.login,
-                color: selectedIndex == 2 ? Colors.blue : Colors.grey,
-              ),
-              label: 'Log in',
+            label: 'Calendar',
+          ),
+          // index = 2
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.login,
+              color: selectedIndex == 2 ? Colors.blue : Colors.grey,
             ),
-          ],
-          selectedItemColor: Colors.green,
-          onTap: _onItemTapped,
-          currentIndex: selectedIndex,
-        ));
+            label: 'Log in',
+          ),
+        ],
+        selectedItemColor: Colors.green,
+        onTap: _onItemTapped,
+        currentIndex: selectedIndex,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+      ),
+    );
   }
 }
