d8cbd05450bb8e2a530546c17634a7603ffb9355
Aleksandra Pacia <olusiaop05@gmail.com>
2023-04-17 23:56:27 +0200

    add class for tile with observations

--- lib/util/obs_tile.dart
+++ lib/util/obs_tile.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+
+class ObsTile extends StatelessWidget {
+  final String whatInf;
+  final String whenInf;
+  final String howInf;
+  Function(BuildContext)? deleteFunction;
+
+  ObsTile( {
+    super.key,
+    required this.whatInf,
+    required this.whenInf,
+    required this.howInf,
+    required this.deleteFunction, 
+
+
+  });
+  @override 
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(10) ,)
+  }
+}
