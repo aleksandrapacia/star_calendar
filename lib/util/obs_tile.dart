412dac161158b8086ea8f140014652b074e916a7
Aleksandra Pacia <olusiaop05@gmail.com>
2023-04-18 22:38:19 +0200

    create container to store user's observation

--- lib/util/obs_tile.dart
+++ lib/util/obs_tile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ObsTile extends StatelessWidget {
   final String whatInf;
@@ -7,18 +7,24 @@ class ObsTile extends StatelessWidget {
   final String howInf;
   Function(BuildContext)? deleteFunction;
 
-  ObsTile( {
+  ObsTile({
     super.key,
     required this.whatInf,
     required this.whenInf,
     required this.howInf,
-    required this.deleteFunction, 
-
-
+    required this.deleteFunction,
   });
-  @override 
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(10) ,)
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: Slidable(
+          child: Container(
+              color: Colors.tealAccent,
+              child: const ListTile(
+                title: Text("whatInf"),
+                subtitle: Text("whenInf"),
+              )),
+        ));
   }
 }
